423a8f150425eaf2b29f57cb7a95913c58212497
hercky <hercs19@gmail.com>
2014-11-02 00:02:01 +0530

    initial svm - going parallel

---
+++ digit_recognizer/svm.R
@@ -0,0 +1,61 @@
+library(caTools)
+library(ggplot2)
+library(caret)
+library(doSNOW)
+# not using the below 
+# library(e1071)
+# library(kernlab)
+
+# Function to randomly split the data
+splitdf <- function(dataframe, nSplits = 2, seed=NULL) {
+    if (!is.null(seed)) set.seed(seed)
+    index <- 1:nrow(dataframe)
+    trainindex <- sample(index, trunc(length(index)/nSplits))
+    trainset <- dataframe[-trainindex, ]
+    validationset <- dataframe[trainindex, ]
+    list(trainset=trainset,validationset=validationset)
+}
+
+train = read.csv('./data/train.csv', header=TRUE)
+test = read.csv('./data/test.csv' , header=TRUE)
+
+## only look at first 1000 observations
+smallTest <- test[1:1000,]
+smallTrain <- train[1:1000,]  
+
+# create the validation set by splitting the training set up into 66% / 33%
+splits <- splitdf(smallTrain, nSplits=3, seed=808)
+trainingSet <- splits$trainset
+validationSet  <- splits$validationset
+
+# take the first column of the training/validation set for y values, the rest for x values
+yTrainingLabels <- as.factor(trainingSet[,1])
+xTrain <- trainingSet[,-1]
+yValLabels <- as.factor(validationSet[,1])
+xVal <- validationSet[,-1]
+yFullTrainingLabels <- as.factor(train[,1])
+xFullTrain <- train[,-1]
+
+
+# Remove columns with zero variance
+zero_var_test  <- nearZeroVar(smallTest)
+zero_var_train <- nearZeroVar(smallTrain)
+xTrain    <- xTrain[,-zero_var_train]
+xVal      <- xVal[,-zero_var_train]
+xTrainFull <- xFullTrain[,-zero_var_train]
+TestFull   <- test[,-zero_var_train]
+
+
+
+
+#Parallize
+nCores <- 2
+c1 <- makeCluster(nCores, type = "SOCK")
+registerDoSNOW(c1) 
+clusterExport(c1, c("%do%","foreach"))
+
+## load libraries on workers
+clusterEvalQ(c1, library(caret)) 
+
+stopCluster(c1)
+
