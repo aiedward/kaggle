bc48e49645989ce1b7d80cf74f7b3577c6f8a718
hercky <hercs19@gmail.com>
2015-04-07 23:32:10 +0530

    fixed a bug for test file

--- restaurant_revenue_prediction/basic_submission.R
+++ restaurant_revenue_prediction/basic_submission.R
@@ -10,7 +10,6 @@ train$Open.Date <- NULL
 train$Id <- NULL
 
 
-
 # convert factor to binary vector
 dummyCity <- model.matrix(~City,data=train)[,-1]
 dummyCity.Group <- model.matrix(~City.Group,data=train)[,-1]
@@ -29,12 +28,12 @@ cv.lm(df = train, fit,m = 10)
 cvlm <- cv.lm(df = train, fit,m = 10)
 
 # test file
-train <- read.csv("~/code/kaggle/stage/data/train.csv")
+test <- read.csv("~/code/kaggle/stage/data/test.csv")
 # convert date to age and log transform
-train$Open.Date <- as.Date(train$Open.Date , format = "%m/%d/%Y")
-train$Age <- log(as.numeric(Sys.Date() - train$Open.Date))
-train$Open.Date <- NULL
-train$Id <- NULL
+test$Open.Date <- as.Date(test$Open.Date , format = "%m/%d/%Y")
+test$Age <- log(as.numeric(Sys.Date() - test$Open.Date))
+test$Open.Date <- NULL
+test$Id <- NULL
 fit <- lm( revenue ~ . , data = train)
 fit <- lm( revenue ~ . , data = train)
 test$City <- NULL
@@ -44,4 +43,8 @@ r_score <- predict(fit,test)
 cbind(r_id,r_score)
 write.csv(cbind(r_id,r_score),file="test_submission.csv",row.names= FALSE)
 
+
+test$Open.Date <- as.Date(test$Open.Date , format = "%m/%d/%Y")
+test$Age <- log(as.numeric(Sys.Date() - test$Open.Date))
+test$Open.Date <- NULL
 
